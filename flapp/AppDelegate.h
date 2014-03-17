d39b4c6d09a205bbf1fbd003128c337571250c52
中山桂一 <nakayamakeiichi@nakayamakeiichi-no-MacBook-Pro.local>
2014-03-18 00:10:56 +0900

    Initial Commit

---
+++ flapp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  flapp
+//
+//  Created by 中山桂一 on 2014/03/18.
+//  Copyright (c) 2014年 Pocket Solution Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ flapp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  flapp
+//
+//  Created by 中山桂一 on 2014/03/18.
+//  Copyright (c) 2014年 Pocket Solution Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
